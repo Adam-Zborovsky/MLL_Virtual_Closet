c057a7ca73716b1282eb5b8bd3c31b289e1c37f6
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2023-11-20 15:35:05 +0200

    1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,8 +5,10 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,8 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
         val switch = findViewById<SwitchCompat>(R.id.switchOnOff)
         val containerRL = findViewById<RelativeLayout>(R.id.idRLContainer)
+        val toolbar = findViewById<Toolbar>(R.id.idToolbar)
+        val naviview = findViewById<NavigationView>(R.id.idNaviView)
         val tvSwitchShahar = findViewById<TextView>(R.id.tvSwitchYes)
         val tvSwitchAdam = findViewById<TextView>(R.id.tvSwitchNo)
         containerRL.background =
@@ -23,11 +27,14 @@ class MainActivity : ComponentActivity() {
             if (switch.isChecked) {
                 containerRL.background =
                     ResourcesCompat.getDrawable(resources, R.drawable.background_shahar, null)
+                naviview.background = containerRL.background
                 tvSwitchShahar.setTextColor(ContextCompat.getColor(this,R.color.white))
                 tvSwitchAdam.setTextColor(ContextCompat.getColor(this,R.color.blue))
+
             } else {
                 containerRL.background =
                     ResourcesCompat.getDrawable(resources, R.drawable.background_adam, null)
+                naviview.background = containerRL.background
                 tvSwitchShahar.setTextColor(ContextCompat.getColor(this,R.color.blue))
                 tvSwitchAdam.setTextColor(ContextCompat.getColor(this,R.color.white))
 
