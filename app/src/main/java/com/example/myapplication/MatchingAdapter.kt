7cd8fa979b5a6b35d18eb0b44aadebf3c0caef0e
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2024-01-02 18:58:03 +0200

    1

--- app/src/main/java/com/example/myapplication/MatchingAdapter.kt
+++ app/src/main/java/com/example/myapplication/MatchingAdapter.kt
@@ -37,9 +37,12 @@ class MatchingAdapter(private var mList: ArrayList<String>, private var switch: 
         holder.details.text = "${clothsList[0]}\nAdam : ${clothsList[3]}\nShahar: ${clothsList[2]}"
 
         if (switch) {
-            if (oldMatching.isNotEmpty()){
-            holder.addBox.isChecked = oldMatching.none { it.split(",")[0] != clothsList[0] }}
-
+            if (oldMatching.isNotEmpty()) {
+                for (i in oldMatching) {
+                    if (i.split(",")[0] == clothsList[0]) {
+                        holder.addBox.isChecked = oldMatching.none { it.split(",")[0] != clothsList[0] }}
+                }
+            }
             holder.addBox.setOnCheckedChangeListener { _, isChecked ->
                 uploadMatching(clothsList, isChecked)
             }
