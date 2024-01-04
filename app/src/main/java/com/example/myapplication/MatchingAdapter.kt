f7784b654c46233cd051cb987db84e92ea3c7a2d
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2024-01-04 14:43:50 +0200

    1

--- app/src/main/java/com/example/myapplication/MatchingAdapter.kt
+++ app/src/main/java/com/example/myapplication/MatchingAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.app.AlertDialog
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -18,21 +21,24 @@ class MatchingAdapter(private var mList: ArrayList<String>, private var switch: 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.matching, parent, false)
+        val holder = ViewHolder(view)
 
         val add = view.findViewById<CheckBox>(R.id.addBox)
         if (switch){add.visibility = View.VISIBLE}
+        val prodImage = view.findViewById<ImageButton>(R.id.prodImage)
+        prodImage.setOnClickListener { bigPicture(mList[holder.adapterPosition], view) }
 
-        return ViewHolder(view)
+        return holder
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val clothsList = mList[position].split(",").map { it.trim() }
-        Log.e("clothlist",clothsList.toString())
 
 
         Glide.with(holder.itemView.context)
             .asDrawable()
             .load(clothsList[4])
             .into(holder.prodImage)
+
 
         holder.details.text = "${clothsList[0]}\nAdam : ${clothsList[3]}\nShahar: ${clothsList[2]}"
 
@@ -45,6 +51,24 @@ class MatchingAdapter(private var mList: ArrayList<String>, private var switch: 
                 uploadMatching(clothsList, isChecked)
             }
         }
+    }
+
+    private fun bigPicture(clothsList: String, view: View) {
+        val builder = AlertDialog.Builder(view.context)
+        val inflater: LayoutInflater = LayoutInflater.from(view.context)
+        val dialogLayout = inflater.inflate(R.layout.big_picutre, null)
+        val clothsList = clothsList.split(",").map { it.trim() }
+
+        Log.e("clothsList",clothsList.toString())
+        builder.setView(dialogLayout)
+
+        val bigPicture = dialogLayout.findViewById<ImageView>(R.id.big_picture)
+        Glide.with(view)
+            .asDrawable()
+            .load(clothsList[4])
+            .centerInside()
+            .into(bigPicture)
+        builder.show()
     }
 
 
@@ -96,7 +120,7 @@ class MatchingAdapter(private var mList: ArrayList<String>, private var switch: 
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val prodImage: ImageView = this.itemView.findViewById(R.id.prodImage)
+        val prodImage: ImageButton = this.itemView.findViewById(R.id.prodImage)
         val details: TextView = this.itemView.findViewById(R.id.details)
         val addBox: CheckBox = this.itemView.findViewById(R.id.addBox)
     }
