96b50fe36a6bdf9b8ff8af93dbc73c0bee99c1fc
Adam Zborovsky <96923903+Adam-Zborovsky@users.noreply.github.com>
2024-02-17 13:41:12 +0200

    1

--- app/src/main/java/GOLD/MLL/VirtualCloset/DataRepository.kt
+++ app/src/main/java/GOLD/MLL/VirtualCloset/DataRepository.kt
@@ -113,29 +113,38 @@ class DataRepository(context: Context) {
         return newList
     }
     suspend fun updateProduct(updates: HashMap<String, Serializable>, clothsItem: Cloths): Cloths {
-        val oldDocRef = db.collection(clothsItem.typeCloth).document(clothsItem.name)
-        oldDocRef.delete()
-        val fileRefByUrl = FirebaseStorage.getInstance().getReferenceFromUrl(clothsItem.photoUrl)
-        val newFileRef = FirebaseStorage.getInstance().reference.child("${updates["Folder"].toString()}/${updates["Name"].toString()}")
+        val docRef = db.collection(clothsItem.typeCloth).document(clothsItem.name)
 
-        val bytes = fileRefByUrl.getBytes(Long.MAX_VALUE).await()
-        newFileRef.putBytes(bytes).await()
-        fileRefByUrl.delete()
-        val newUrl = newFileRef.downloadUrl.await()
-        updates["URL"] = newUrl.toString()
+        if (updates["Name"] != clothsItem.name) {
+            docRef.delete()
+            val fileRefByUrl =
+                FirebaseStorage.getInstance().getReferenceFromUrl(clothsItem.photoUrl)
+            val newFileRef =
+                FirebaseStorage.getInstance().reference.child("${updates["Folder"].toString()}/${updates["Name"].toString()}")
 
-        if (clothsItem.backsideUrl != "") {
-            val fileRefByUrl = FirebaseStorage.getInstance().getReferenceFromUrl(clothsItem.backsideUrl)
-            val newFileRef = FirebaseStorage.getInstance().reference.child("BackSide/${updates["Folder"].toString()}/${updates["Name"].toString()}")
             val bytes = fileRefByUrl.getBytes(Long.MAX_VALUE).await()
             newFileRef.putBytes(bytes).await()
             fileRefByUrl.delete()
             val newUrl = newFileRef.downloadUrl.await()
-            updates["BackSide"] = newUrl.toString()
-        }
+            updates["URL"] = newUrl.toString()
 
-        val newDocRef = db.collection(updates["Folder"].toString()).document(updates["Name"].toString())
-        newDocRef.set(updates)
+            if (clothsItem.backsideUrl != "") {
+                val fileRefByUrl =
+                    FirebaseStorage.getInstance().getReferenceFromUrl(clothsItem.backsideUrl)
+                val newFileRef =
+                    FirebaseStorage.getInstance().reference.child("BackSide/${updates["Folder"].toString()}/${updates["Name"].toString()}")
+                val bytes = fileRefByUrl.getBytes(Long.MAX_VALUE).await()
+                newFileRef.putBytes(bytes).await()
+                fileRefByUrl.delete()
+                val newUrl = newFileRef.downloadUrl.await()
+                updates["BackSide"] = newUrl.toString()
+            }
+            val newDocRef = db.collection(updates["Folder"].toString()).document(updates["Name"].toString())
+            newDocRef.set(updates)
+        }
+        else {
+            docRef.update(updates as Map<String, Any>)
+        }
 
         val newProduct = Cloths(updates["Name"].toString(), updates["Folder"].toString(), updates["ShaharLikes"].toString().toInt(), updates["AdamLikes"].toString().toInt(), updates["URL"].toString(), updates["BackSide"].toString(), updates["matching"] as ArrayList<String>)
         val fullList = getCachedProducts()!!.toMutableList()
@@ -152,19 +161,18 @@ class DataRepository(context: Context) {
             }
         }
 
-        Log.e("URL",updates["URL"].toString())
+        Log.e("clothsToEdit",clothsToEdit.toString())
         iterator = clothsToEdit.iterator()
         while (iterator.hasNext()) {
             val i = iterator.next()
-            val docRef = db.collection(i.typeCloth).document(i.name)
+            val changeDocRef = db.collection(i.typeCloth).document(i.name)
             i.matching.remove("${clothsItem.name},${clothsItem.typeCloth},${clothsItem.sLike},${clothsItem.aLike},${clothsItem.photoUrl},${clothsItem.backsideUrl},${clothsItem.matching}")
             i.matching.add("${newProduct.name},${newProduct.typeCloth},${newProduct.sLike},${newProduct.aLike},${newProduct.photoUrl},${newProduct.backsideUrl},${newProduct.matching}")
-            docRef.update("matching", i.matching)
+            changeDocRef.update("matching", i.matching)
         }
 
 
         cacheProducts(fullList)
-        cacheOneProduct(newProduct)
         return newProduct
 
     }
